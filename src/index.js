f0c1b7602d2ceba775818e725be310e994e3c3a3
ihda06 <ihdaanwari5@gmail.com>
2023-09-20 18:51:53 +0700

    try add express

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ const { logsRequest } = require("./middleware/logs");
 const session = require("telegraf/session");
 const { stage } = require("./stages");
 require('dotenv').config();
+const express = require('express')
+const expressApp = express()
 
 const bot = new Telegraf(process.env.API_TOKEN)
 console.log("check api token", process.env.API_TOKEN);
+
+
+
+// const { Telegraf } = require('telegraf')
+
+
+expressApp.use(bot.webhookCallback('/secret-path'))
+bot.telegram.setWebhook('https://vast-jade-angelfish-hat.cyclic.cloud/secret-path')
+
+expressApp.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 bot.use(logsRequest);
 bot.use(session());
@@ -17,9 +31,6 @@ bot.start((ctx) => {
 // bot.on("text", (ctx) => ctx.reply("Hello World"));
 bot.launch();
 
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     console.log(`Just got a request at ${req.url}!`)
-//     res.write('Yo!');
-//     res.end();
-// }).listen(process.env.PORT || 3000);
+expressApp.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
