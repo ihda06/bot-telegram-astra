fb0f1f97f5e1999cc0410cd07cae9f364556507e
ihda06 <ihdaanwari5@gmail.com>
2023-09-20 19:58:11 +0700

    try to deploy

--- src/index.js
+++ src/index.js
@@ -4,35 +4,44 @@ const session = require("telegraf/session");
 const { stage } = require("./stages");
 require('dotenv').config();
 const express = require('express')
-const expressApp = express()
+
 
 const bot = new Telegraf(process.env.API_TOKEN)
 console.log("check api token", process.env.API_TOKEN);
 
 
+bot.use(logsRequest);
+bot.use(session());
+bot.use(stage.middleware());
+
+// bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
 
 // const { Telegraf } = require('telegraf')
 
 
-expressApp.use(bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook('https://vast-jade-angelfish-hat.cyclic.cloud/secret-path')
+// bot.telegram.setWebhook('https://vast-jade-angelfish-hat.cyclic.cloud/secret-path')
 
-expressApp.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-bot.use(logsRequest);
-bot.use(session());
-bot.use(stage.middleware());
-bot.start((ctx) => {
-  ctx.scene.enter("welcome");
-});
 
-// bot.on("text", (ctx) => ctx.reply("Hello World"));
-bot.launch().then(() => {
-  console.log('Bot is running!');
-});;
+// // bot.on("text", (ctx) => ctx.reply("Hello World"));
+// bot.launch().then(() => {
+//   console.log('Bot is running!');
+// });;
 
-expressApp.listen(3000, () => {
-  console.log('Example app listening on port 80!')
-})
+if (process.env.NODE_ENV === "production") {
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  bot.start((ctx) => {
+    ctx.scene.enter("welcome");
+  });
+}
+
