c472671edc29fb5d69092f41a447e92b1d05d407
ihda06 <ihdaanwari5@gmail.com>
2023-09-20 20:10:11 +0700

    hzz eror mulu bjir

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ const { default: Telegraf } = require("telegraf");
 const { logsRequest } = require("./middleware/logs");
 const session = require("telegraf/session");
 const { stage } = require("./stages");
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 
+const bot = new Telegraf(process.env.API_TOKEN);
+console.log("NODE ENV", process.env.NODE_ENV);
+console.log("ini production");
+const app = express();
+app.use(express.json());
+app.use(webhookCallback(bot, "express"));
 
-const bot = new Telegraf(process.env.API_TOKEN)
-console.log("NODE ENV",process.env.NODE_ENV);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Bot listening on port ${PORT}`);
+});
 
 // bot.use(logsRequest);
 // bot.use(session());
@@ -16,33 +24,19 @@ console.log("NODE ENV",process.env.NODE_ENV);
 // // bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 // bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
-
 // const { Telegraf } = require('telegraf')
 
-
 // bot.telegram.setWebhook('https://vast-jade-angelfish-hat.cyclic.cloud/secret-path')
-
-
 
 // // bot.on("text", (ctx) => ctx.reply("Hello World"));
 // bot.launch().then(() => {
 //   console.log('Bot is running!');
 // });;
 
-if (process.env.NODE_ENV === "production") {
-  console.log("ini production");
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
-  
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  console.log("ini dev");
-  bot.start((ctx) => {
-    ctx.scene.enter("welcome");
-  });
-}
-
+// if (process.env.NODE_ENV === "production") {
+// } else {
+//   console.log("ini dev");
+//   bot.start((ctx) => {
+//     ctx.scene.enter("welcome");
+//   });
+// }
