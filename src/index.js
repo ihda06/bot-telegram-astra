c954b8cbcae05b1330fa9fad2b4418fac54cdb57
ihda06 <ihdaanwari5@gmail.com>
2023-09-22 14:22:00 +0700

    try di main

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ const { stage } = require("./stages");
 
 const { Telegraf } = require("telegraf");
 const express = require("express");
+const Airtables = require("./utils/Airtable");
+const transformData = require("./utils/TransformData");
 
 require("dotenv").config();
 
 const expressApp = express();
 
-const bot = new Telegraf(process.env.PRODUCTION === "TRUE" ? process.env.API_TOKEN : process.env.API_TOKEN_DEV);
+const randomizer = (data) => {
+  const randomNumber = Math.floor(Math.random() * data.length);
+  return data[randomNumber];
+};
+
+const bot = new Telegraf(
+  process.env.PRODUCTION === "TRUE"
+    ? process.env.API_TOKEN
+    : process.env.API_TOKEN_DEV
+);
 
 bot.use(logsRequest);
 bot.use(session());
@@ -17,11 +28,36 @@ bot.use(stage.middleware());
 // Register middleware
 bot.start((ctx) => {
   ctx.scene.enter("welcome");
-  if(ctx.message){
+  if (ctx.message) {
     ctx.session.state = { userInfo: ctx.message.from };
   }
 });
-bot.telegram.webhookReply = false
+bot.command("tes", async (ctx) => {
+  try {
+    console.log("getting message..");
+    let rawdata = await Airtables("TopikGenerator").select().all();
+    console.log("message dapet");
+    rawdata = transformData(rawdata);
+    const topik = randomizer(rawdata);
+    ctx.reply(
+      `Berikut topik pembicaraan yang mungkin cocok untuk kamu bahas dengan teman atau pasangan kamu\n \n${topik.Topik}`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            /* One button */
+            [{ text: "Tambah Topik", callback_data: "TambahTopik" }],
+            [{ text: "Menu", callback_data: "menu" }],
+            [{ text: "Topik Lain", callback_data: "TopikGenerator" }],
+          ],
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    ctx.reply("error");
+  }
+});
+// bot.telegram.webhookReply = false
 bot.on("message", (ctx) => ctx.reply("Tolong isi sesuai format ya"));
 if (process.env.PRODUCTION === "TRUE") {
   expressApp.use(bot.webhookCallback("/"));
