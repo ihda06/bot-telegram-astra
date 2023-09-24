c9cbf84ffbbfa811fae7b0f831899c2df27ce5d2
ihda06 <ihdaanwari5@gmail.com>
2023-09-24 12:17:23 +0700

    error arrangement

--- src/command/twitterBot.js
+++ src/command/twitterBot.js
@@ -50,9 +50,10 @@ const register = async (ctx) => {
         },
       },
     ]);
-    ctx.reply(
+    await ctx.reply(
       "Data berhasil disimpan ✅✅ \n\nData kamu perlu diverifikasi oleh admin terlebih dahulu, coba ulang nanti ya"
     );
+    ctx.scene.enter("welcome")
   } catch (error) {
     console.log(error);
     ctx.reply("error");
